7804e5d9df0e3887e2c5031260afd7a24d0930c6
Archantum <86094701+Archantum@users.noreply.github.com>
2022-08-14 14:46:04 +0700

    Add files via upload

---
+++ swarm/devel/rosserial/rosserial/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bernard/swarm/src/rosserial/rosserial
+# Build directory: /home/bernard/swarm/build/rosserial/rosserial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
