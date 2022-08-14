0bbfbb29291ce570ff3da4a279182f7d2c8f2482
Archantum <86094701+Archantum@users.noreply.github.com>
2022-08-14 14:39:16 +0700

    Add files via upload

---
+++ swarm/devel/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/bernard/swarm/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/bernard/swarm/devel;/home/bernard/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/bernard/swarm/devel;/home/bernard/catkin_ws/devel;/opt/ros/melodic' != '' else []
