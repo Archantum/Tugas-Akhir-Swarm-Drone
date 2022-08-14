0d4dfa0723d0ab9557b924b9f167d23c5be17994
Archantum <86094701+Archantum@users.noreply.github.com>
2022-08-14 14:54:28 +0700

    Add files via upload

---
+++ swarm/devel/rosserial/rosserial_client/catkin_generated/python_distutils_install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+if [ -n "$DESTDIR" ] ; then
+    case $DESTDIR in
+        /*) # ok
+            ;;
+        *)
+            /bin/echo "DESTDIR argument must be absolute... "
+            /bin/echo "otherwise python's distutils will bork things."
+            exit 1
+    esac
+fi
+
+echo_and_run() { echo "+ $@" ; "$@" ; }
+
+echo_and_run cd "/home/bernard/swarm/src/rosserial/rosserial_client"
+
+# ensure that Python install destination exists
+echo_and_run mkdir -p "$DESTDIR/home/bernard/swarm/install/lib/python2.7/dist-packages"
+
+# Note that PYTHONPATH is pulled from the environment to support installing
+# into one location when some dependencies were installed in another
+# location, #123.
+echo_and_run /usr/bin/env \
+    PYTHONPATH="/home/bernard/swarm/install/lib/python2.7/dist-packages:/home/bernard/swarm/build/lib/python2.7/dist-packages:$PYTHONPATH" \
+    CATKIN_BINARY_DIR="/home/bernard/swarm/build" \
+    "/usr/bin/python2" \
+    "/home/bernard/swarm/src/rosserial/rosserial_client/setup.py" \
+     \
+    build --build-base "/home/bernard/swarm/build/rosserial/rosserial_client" \
+    install \
+    --root="${DESTDIR-/}" \
+    --install-layout=deb --prefix="/home/bernard/swarm/install" --install-scripts="/home/bernard/swarm/install/bin"
